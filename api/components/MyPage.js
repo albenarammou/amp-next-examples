dbadbcba5965eb40ea124aa8e208c795d5c97d3f
Alan Kent <alan.james.kent@gmail.com>
2019-05-03 19:12:43 -0700

    Added a bit of CSS to center things.

--- api/components/MyPage.js
+++ api/components/MyPage.js
@@ -2,12 +2,16 @@ import AllProducts from '../components/AllProducts.js';
 
 export default () => (
   <div className="page">
-    <h1>Welcome to AMP!</h1>
-    <p><amp-img src="/static/amp-logo.png" layout="fixed" width="50" height="50" /></p>
+    <h1 className="center">Welcome to AMP!</h1>
+    <p className="center"><amp-img src="/static/amp-logo.png" layout="fixed" width="50" height="50" /></p>
     <AllProducts />
     <style jsx>{`
       .page {
         padding: 0.5em;
+      }
+      .center {
+        display: flex;
+        justify-content: center;
       }
     `}</style>
   </div>
--- first/components/MyPage.js
+++ first/components/MyPage.js
@@ -1,6 +1,13 @@
 export default () => (
   <div>
-    <h1>Welcome to AMP!</h1>
-    <p><amp-img src="/static/amp-logo.png" layout="fixed" width="225" height="225" /></p>
+    <h1 className="center">Welcome to AMP!</h1>
+    <p className="center"><amp-img src="/static/amp-logo.png" layout="fixed" width="50" height="50" /></p>
+
+    <style jsx>{`
+      .center {
+        display: flex;
+        justify-content: center;
+      }
+    `}</style>
   </div>
 );
